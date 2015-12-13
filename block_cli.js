7cd4536b94770211a5772d30127afef39faffe86
Simon S. Soriano <simon0191@gmail.com>
2015-12-13 16:22:25 -0500

    Implement github login

--- block_cli.js
+++ block_cli.js
@@ -1,8 +1,13 @@
 import program from 'commander'
 import path from 'path'
+import inquirer from 'inquirer'
+import Octokat from 'octokat'
+import _ from 'lodash'
+import fs from 'fs'
 
 import BlockRetriever from './block_retriever'
 import BlockSaver from './block_saver'
+import Utils from './utils'
 
 const BLOCK_LOCATION_REGEX = /^([\.\w-]+)\/([\.\w-]+)\:([\.\w-]+)$/i
 
@@ -13,9 +18,12 @@ export default class BlockCli {
 
   get(githubUser,githubRepo,blockName) {
 
-    const blockRetriever = new BlockRetriever(githubUser,githubRepo,blockName,process.env.GITHUB_TOKEN)
-    const blockSaver = new BlockSaver(path.join(githubUser,githubRepo))
-    return blockSaver.save(blockRetriever.files())
+    let readFile = Utils.promisify(fs,fs.readFile)
+    readFile(self.tokenFilePath()).then((token) => {
+      const blockRetriever = new BlockRetriever(githubUser,githubRepo,blockName,token || process.env.GITHUB_TOKEN || '')
+      const blockSaver = new BlockSaver(path.join(githubUser,githubRepo))
+      return blockSaver.save(blockRetriever.files())
+    })
   }
   parseGetOption(option) {
     let match = this.blockLocationRegex().exec(option)
@@ -32,15 +40,46 @@ export default class BlockCli {
         console.log(err)
       })
   }
+
+  loginCommand() {
+    const questions = [
+      {type: 'input',name: 'username',message: 'Github username'},
+      {type: 'password',name: 'password',message: 'Github password'},
+    ]
+    inquirer.prompt(questions, (answers) => {
+      this.loginWithGithub(answers['username'],answers['password'])
+    });
+  }
+
+  loginWithGithub(username,password) {
+    const octo = new Octokat({username:username,password:password})
+    octo.authorizations
+      .create({note: 'Authorization for block cli',fingerprint:Utils.randomString()})
+      .then(_.property('token'))
+      .then(_.partial(Utils.promisify(fs,fs.writeFile),this.tokenFilePath()))
+      .then(_.constant('Authenticated'))
+      .then(console.log)
+      .catch(console.log)
+
+  }
+
   run() {
     program
       .version('0.0.1')
       .option('-g, --get <url>', 'Location of the block',this.blockLocationRegex())
+      .option('-l, --login', 'Login with github')
       .parse(process.argv)
 
-    if (program.get) {
+    if(program.get) {
       this.getCommand(program.get)
+    } else if(program.login) {
+      this.loginCommand()
     }
+  }
+
+  //private
+  tokenFilePath() {
+    return '.gh_token'
   }
 
 }
--- utils.js
+++ utils.js
@@ -13,4 +13,17 @@ export default class Utils {
       })
     }
   }
+
+  static randomString() {
+    const dic = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    return _.range(0,20)
+      .map(_.partial(_.random,0,dic.length-1,false))
+      .map(_.partial(this.pick,dic))
+      .join('')
+  }
+
+  static pick(arr,pos) {
+    return arr[pos]
+  }
 }
